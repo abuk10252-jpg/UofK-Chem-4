c6bfe865a04f3124a2316cd5f0c2c90b9fb06def
abuk10252-jpg <abuk10252@gmail.com>
2026-05-02 00:23:57 +0200

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,38 +1,14 @@
 import { Stack } from 'expo-router';
-import { AuthProvider, useAuth } from '../src/context/AuthContext';
 import { StatusBar } from 'expo-status-bar';
-import LoadingScreen from '../src/components/LoadingScreen';
-import { useEffect } from 'react';
-import * as SplashScreen from 'expo-splash-screen';
-
-SplashScreen.preventAutoHideAsync();
-
-function RootLayoutNav() {
-  const { loading } = useAuth();
-
-  useEffect(() => {
-    if (!loading) {
-      SplashScreen.hideAsync().catch(() => {});
-    }
-  }, [loading]);
-
-  if (loading) {
-    return <LoadingScreen />;
-  }
-
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="login" />
-    </Stack>
-  );
-}
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
+    <>
       <StatusBar style="light" />
-      <RootLayoutNav />
-    </AuthProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login" />
+      </Stack>
+    </>
   );
 }
